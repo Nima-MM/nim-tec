a87c47d165d1d05b0e6c808fd3968789383d35d8
gitNimad <marashi.nima@outlook.de>
2025-04-30 19:51:05 +0200

    add raw data/content for later implementation in website

---
+++ src/app/ressources/data.ts
@@ -0,0 +1,126 @@
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projekte",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Chronologie",
+    hash: "#chronology",
+  },
+  {
+    name: "Kontakt",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "QA in Kommissionierung",
+    location: "Cologne",
+    description:
+      "Einsatz bei Igus: Ich prüfte Produkte auf Maßhaltigkeit und Qualität vor der Kommissionierung.",
+    icon: "IGUS-ICON Later oder QA-Komissionierung ICON",
+    date: "01/2021 - 10/2022",
+  },
+  {
+    title: "Fachinformatiker Anwendungsentwicklung",
+    location: "Cologne",
+    description:
+      "Während meiner Umschulung habe ich fundierte theoretische Kenntnisse in diversen Bereichen der IT erlangt.",
+    icon: "AFP-ICON Later oder SchulIcon",
+    date: "01/2023 - 01/2025",
+  },
+  {
+    title: "Frontend-Entwickler",
+    location: "Dortmund",
+    description:
+      "Frontend bei IA International: Mit JavaScript, HTML/CSS und Shopify Liquid entwickelte ich neue Shop-Seiten für Sugargang.com, optimierte die UX und vertiefte agile Zusammenarbeit.",
+    icon: "IA-ICON Later oder Webdev/FrontedIcon",
+    date: "09/2023 - 04/2024",
+  },
+  {
+    title: "Frontend-Entwickler",
+    location: "Leverkusen",
+    description:
+      "Frontend bei IA International: Mit JavaScript, HTML/CSS und Shopify Liquid entwickelte ich neue Shop-Seiten für Sugargang.com, optimierte die UX und vertiefte agile Zusammenarbeit.",
+    icon: "HoomIT-ICON oder Fullstack-ICNLater",
+    date: "05/2024 - 02/2025",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Website nim-tec",
+    description:
+      "Meine aktuelle Website, dass mich und mein Portfolio zur Schau stellt.",
+    tags: ["TypeScript", "Next.js", "Cypress", "Tailwind", "Prisma"],
+    date: "05/2025 - aktuell",
+    imageUrl: "Verlinkung zu Github oder live Demo",
+  },
+  {
+    title: "StockEase",
+    description:
+      "Eine simple, intuitive Applikation zur Kontrolle eines einfachen Lagers auf alle Plattformen.",
+    tags: ["Java","TypeScript", "Vue.js", "PostgreSQL", "Tailwind", "Vite", "Vitest", "Spring-Boot", "Maven", "Docker"],
+    date: "04/2025 - aktuell",
+    imageUrl: "Verlinkung zu Github oder live Demo",
+  },
+  {
+    title: "Lagerkontrolle",
+    description:
+      "LEGACY. Wird nicht fortgeführt. Eine Webapp zur Kontrolle eines einfachen Lagers im Browser.",
+      tags: ["Java","TypeScript", "Vue.js", "PostgreSQL", "Vite", "Vitest", "Spring-Boot", "Maven", "Docker"],
+      date: "01/2025 - legacy",
+    imageUrl: "Verlinkung zu Github oder live Demo",
+  },
+  {
+    title: "G€LDSAGA",
+    description:
+      "Konzeptionierungsphase. Eine App, die Nutzern ermöglichen soll, fixe und spontane Ausgaben, sowie Einnahmen schnell und unkompliziert zu erfassen.",
+    tags: ["TypeScript", "React Native", "Figma"],
+    date: "01/2025 - frozen",
+    imageUrl: "Verlinkung zu Github oder live Demo",
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "SQL",
+  "Vite",
+  "React",
+  "Next.js",
+  "Vue.js",
+  "Nuxt.js",
+  "Bootstrap",
+  "Sass",
+  "Tailwind",
+  "PostgreSQL",
+  "Redux",
+  "Cypress",
+  "Sinon.js",
+  "Jest",
+  "Vitest",
+  "PostgreSQL",
+  "Git",
+    "GitHub",
+  "Jira",
+  "Miro",
+  "Figma",
+  "LaTeX",
+] as const;
